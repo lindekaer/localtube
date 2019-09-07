5b711097fdd0cf93c47904523a2ceec121ffdd86
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2019-09-07 23:09:33 +0200

    Ensure that node file can be run from any path

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
+const path = require('path')
 const queryString = require('query-string')
 const _uniq = require('lodash/uniq')
 const shell = require('shelljs')
 
 const FILE_PATH_BOOKMARKS = '/Users/theo/Library/Application Support/Google/Chrome/Default/Bookmarks'
-const FILE_PATH_ERROR_LOG = 'error-log.json'
-const FILE_PATH_SONGS = 'songs'
+const FILE_PATH_ERROR_LOG = path.join(__dirname, 'error-log.json')
+const FILE_PATH_SONGS = path.join(__dirname, 'songs')
 
 const bookmarksFile = JSON.parse(fs.readFileSync(FILE_PATH_BOOKMARKS))
 const songsArray = bookmarksFile.roots.bookmark_bar.children[4].children
@@ -20,29 +21,33 @@ const songs = _uniq(
 )
 
 const getCommand = id =>
-  `cd songs && youtube-dl --extract-audio --audio-format mp3 'https://www.youtube.com/watch?v=${id}'`
+  `cd ${FILE_PATH_SONGS} && youtube-dl --extract-audio --audio-format mp3 'https://www.youtube.com/watch?v=${id}'`
 
-shell.exec(`brew upgrade youtube-dl`)
+try {
+  shell.exec(`brew upgrade youtube-dl`)
 
-for (let { id, name } of songs) {
-  const errorLog = JSON.parse(fs.readFileSync(FILE_PATH_ERROR_LOG))
-  const currentFileNames = fs.readdirSync(FILE_PATH_SONGS)
+  for (let { id, name } of songs) {
+    const errorLog = JSON.parse(fs.readFileSync(FILE_PATH_ERROR_LOG))
+    const currentFileNames = fs.readdirSync(FILE_PATH_SONGS)
 
-  const fileExists = currentFileNames.some(file => file.includes(id))
-  const fileHasError = errorLog[id] != null
+    const fileExists = currentFileNames.some(file => file.includes(id))
+    const fileHasError = errorLog[id] != null
 
-  if (fileExists || fileHasError) {
-    console.log(`Skipping ${name}`)
-  } else {
-    const result = shell.exec(getCommand(id))
-    if (result.code !== 0) {
-      errorLog[id] = { error: result.stderr || 'N/A', name }
+    if (fileExists || fileHasError) {
+      console.log(`Skipping ${name}`)
     } else {
-      console.log(`${name} downloaded!`)
+      const result = shell.exec(getCommand(id))
+      if (result.code !== 0) {
+        errorLog[id] = { error: result.stderr || 'N/A', name }
+      } else {
+        console.log(`${name} downloaded!`)
+      }
     }
+
+    fs.writeFileSync(FILE_PATH_ERROR_LOG, JSON.stringify(errorLog, null, 2))
   }
 
-  fs.writeFileSync(FILE_PATH_ERROR_LOG, JSON.stringify(errorLog, null, 2))
+  console.log('✅ Done!')
+} catch (err) {
+  console.error(err)
 }
-
-console.log('✅ Done!')
--- save.sh
+++ save.sh
@@ -1,9 +1,9 @@
 echo "Fetching files..."
 
-node index.js
+node /Users/theo/code/localtube/index.js
 
 echo "Uploading files to bucket..."
 
-gsutil -m cp -r -n songs gs://personal-theo-backup
+gsutil -m cp -r -n /Users/theo/code/localtube/songs gs://personal-theo-backup
 
 echo "Done!"
